027a9fd40692e938f52ddf4dd49540b182609522
Daniel Nikpayuk <djharris@ualberta.ca>
2014-12-23 00:56:15 -0700

    latest commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Wagner-Fischer
+===================
+
+C implementation of a basic Wagner Fischer algorithm (metric) for calculating distances of string similarity.
+
+This particular implementation is derived from the pseudo-code presented in
+
+"Data Structures and Algorithms in C++", Adam Drozdek, Thomson Learning, 2005.
+
+In particular I've optimized for space constraints as the pseudo-code offered a O( |string1| x |string2| )
+computational complexity in space requirements, while I've reduced it to O( |string1| ).  It can further be
+optimized I'm sure, but for my purposes that much is overkill (diminishing returns).
+
+It was compiled under the following command:
+
+gcc -std=gnu99 main.c -o <NAME>
+
+with <NAME> being the obvious intended variable name of the binary.
+
+If you find it useful or have suggestions, let me know. Thanks.
--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*************************************************************************************************************************
+**
+** Copyright 2014 Daniel Nikpayuk
+**
+** This file is part of Wagner_Fischer.
+**
+** Wagner_Fischer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+**
+** Wagner_Fischer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along with Wagner_Fischer. If not, see
+** <http://www.gnu.org/licenses/>.
+**
+*************************************************************************************************************************/
+
 #include<stdio.h>
 #include<string.h>
 
